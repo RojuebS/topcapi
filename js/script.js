f71472425fa0bfd2e667dc2d7335713b21542c92
Rogerio <rogerio@rogeriocustodio.com.br>
2018-02-24 02:47:44 -0300

    validando campos e email

--- js/script.js
+++ js/script.js
@@ -4,4 +4,35 @@ window.addEvent('domready', () => {
             id_manage: this.id
         })
     });
+
+    $('form_subimit').addEvent('submit', function(ev){
+        ev.stop();
+        this.getElements('input, select, textarea').each( (el, n) => {
+            if(el.get('value').trim() === ""){
+                el.getParent().addClass('error');
+            } else {
+                el.getParent().removeClass('error');
+                if(!validateEmail()){
+                    $("email").getParent().addClass('error');
+                }else{
+                    $("email").getParent().removeClass('error');
+                }
+            }
+        });
+
+        if($$('.error').length === 0) {
+           this.submit();
+        }
+    });
 });
+
+function validateEmail(){
+    let email = $("email").get('value');
+    let filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+    if(filtro.test(email)) {
+        return true;
+    }else{
+        return false;
+    }
+}
